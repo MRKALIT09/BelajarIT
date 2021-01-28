dcac14095ede7d9b7451ca85a3726161e215f44a
MedanXploit12 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-28 18:23:54 +0700

    Add files via upload

---
+++ belajarIT.sh
@@ -0,0 +1,128 @@
+#!bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+echo $pur" ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁"
+echo $ku"  AUTHOR SCRIP : $cy</>MANDO XPLOIT$pu ||$me ██████████████████"
+echo $ku"  WHATSAPP ME  : $ku 085361524681  $pu ||$me ██████████████████"
+echo $ku"  GITHUB ME : GITHUB.COM/MRKALIT09 ||$pu ██████████████████     "                                     |
+echo $ku"   MY FACEBOOK : $i Su Mando       $pu||$pu ██████████████████"
+echo $pur" ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁$pu ||"
+echo $pu"                                 ||"
+echo $pu"               	                 ||"
+echo $pu"                                 ||"
+echo $me" 				 =="
+echo -n    "   [?] MASUKAN PASSWORD TOOLS : "
+read pw;
+if [ $pw = 'MANDOXPLOIT' ]
+then
+echo $me"   [!] SEDANG MENGECEK PASSWORD ...    "
+sleep 8
+echo $i"   [✓] LOGIN SUCCES FULL "
+sleep 4
+echo $ku "  JANGAN LUPA FOLLOW AKUN GW YA BRO "
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+else
+echo $me"   [!] SEDANG MENGECEK PASSWORD "
+sleep 8
+echo $me "  [X] PASSWORD SALAH "
+exit
+fi
+echo
+echo " ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁" | lolcat
+echo "     ==[  MASUKAN MENU PILIHAN ANDA  ]==     " | lolcat
+echo " ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁" | lolcat
+echo
+echo $me "[1] $i BELAJAR BAHASA PEMROGRAMAN PYTHON  "
+echo $me "[2] $i BELAJAR BAHASA PEMROGRAMAN PHP     "
+echo $me "[3] $i BELAJAR BAHASA PEMROGRAMAN RUBY    "
+echo $me "[4] $i BELAJAR BAHASA PEMROGRAMAN BASH    "
+echo $me "[5] $i MENGENAL APA ITU METASPLOIT        "
+echo $me "[6] $i PERBEDAAN HACKER DENGAN CRACKER    "
+echo $me "[7] $i FUNSI SEBENARNYA TERMUX            "
+echo $me "[8] $i MENGENAL APA ITU JAVAS SCRIP       "
+echo $me "[9] $bi HUBUNGIN ADMIN VIA FACEBOOK       "
+echo $me "[10]$bi HUBUNGIN ADMIN VIA WHATSAPP       "
+echo " ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁" | lolcat
+read -p " =========[> : " pill
+if [ $pill = "1" ]
+then
+echo $me " [•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB "
+sleep 4
+echo $i " [✓] SELAMAT BELAJAR BRO ^_^"
+sleep 2
+xdg-open https://id.m.wikipedia.org/wiki/Python_bahasa_pemrograman
+sleep 3
+fi
+if [ $pill = "2" ]
+then
+echo $me " [•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB "
+sleep 4
+echo $i " [✓] SELAMAT BELAJAR BRO ^_^"
+sleep 2
+xdg-open https://www.google.com/amp/s/www.niagahoster.co.id/blog/pengertian-php/
+sleep 3
+fi
+if [ $pill = "3" ]
+then
+echo $me " [•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB "
+sleep 4
+echo $i " [✓] SELAMAT BELAJAR BRO ^_^"
+sleep 2
+xdg-open https://id.m.wikipedia.org/wiki/Ruby_bahasa_pemrograman
+fi
+if [ $pill = "4" ]
+then
+echo $me " [•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB"
+sleep 4
+echo $i "  [✓] SELAMAT BELAJAR BRO ^_^ "
+sleep 2
+xdg-open https://www.google.com/amp/s/www.hostinger.co.id/tutorial/bash-script/amp/
+fi
+if [ $pill = "5" ]
+then
+echo $me " [•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB "
+echo $i  "  [✓] SELAMAT BELAJAR BRO "
+xdg-open http://www.jejakhimea.com/2019/12/pengertian-metasploit.html?m=1
+fi
+if [ $pill = "6" ]
+then
+echo $me "[•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB "
+sleep 4
+echo $me " [✓] SELAMAT BELAJAR BRO ^_^ "
+xdg-open https://carisinyal.com/perbedaan-hacker-dan-cracker/
+fi
+if [ $pill = "7" ]
+then
+echo $me "[•] ANDA AKAN DI ARAHKAN SECARA OTOMATIS KE WEB "
+sleep 4
+echo $i  "[✓] SELAMAT BELAJAR BRO ^_^ "
+xdg-open https://qwords.com/blog/termux-adalah/
+fi
+if [ $pill = "8" ]
+then
+echo $me "[•] ANDA AKAN DI ALIHKAN SECARA OTOMATIS KE WEB "
+sleep 4
+echo $i  "[✓] SELAMAT BELAJAR BRO ^_^ "
+sleep 2
+xdg-open https://www.google.com/amp/s/www.hostinger.co.id/tutorial/apa-itu-javascript/amp/
+fi
+if [ $pill = "9" ]
+then
+echo $me "[•] MOHON MENUNGGU ......."
+sleep 5
+xdg-open https://www.facebook.com/profile.php?id=100027700041268
+fi
+if [ $pill = "10" ]
+then
+echo $me "[•] MOHON MENUNGGU ........"
+xdg-open wa.me/085361524681
+fi
+
